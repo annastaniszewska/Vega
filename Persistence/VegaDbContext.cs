4e84a2887c9500ba0cd4d2d2836c4b16a1bae8f1
anna.staniszewska <anna.staniszewska93@gmail.com>
2021-02-23 18:41:37 +0100

    Implement API for retrieving features.

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -6,6 +6,8 @@ namespace vega.Persistence
     public class VegaDbContext : DbContext
     {
         public DbSet<Make> Makes { get; set; }
+
+        public DbSet<Feature> Features { get; set; }
         
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
