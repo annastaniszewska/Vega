ba447c7dda6f54bb48e868f307a4ed38580e4a10
anna.staniszewska <anna.staniszewska93@gmail.com>
2021-02-24 15:32:11 +0100

    Refactoring: merging services.

--- ClientApp/src/services/vehicle.service.ts
+++ ClientApp/src/services/vehicle.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class FeatureService {
+export class VehicleService {
 
   constructor(private http: HttpClient) { }
+
+  getMakes(){
+    return this.http.get('/api/makes')
+    .pipe(map((res: any) => res));
+  }
 
   getFeatures(){
     return this.http.get('/api/features')
