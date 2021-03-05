e3c1e977ce172f29191e2f9ff00778d4b844d93d
anna.staniszewska <anna.staniszewska93@gmail.com>
2021-03-05 17:51:20 +0100

    Add sentry.

--- ClientApp/src/app/app.error-handler.ts
+++ ClientApp/src/app/app.error-handler.ts
@@ -1,11 +1,16 @@
-import { ErrorHandler, Inject, NgZone } from "@angular/core";
+import { ErrorHandler, Inject, NgZone, isDevMode } from "@angular/core";
+import * as Sentry from '@sentry/browser'
 import { ToastrService } from "ngx-toastr";
 
 export class AppErrorHandler implements ErrorHandler {
 
     constructor(
         private ngZone: NgZone,
-        @Inject(ToastrService) private toastr: ToastrService) {}
+        @Inject(ToastrService) private toastr: ToastrService) {
+            Sentry.init({
+                dsn: 'https://1fc215a1e9a54d8aacea89d4dc37c89e@o543405.ingest.sentry.io/5663639'
+              })
+        }
 
     handleError(error: any): void {
         this.ngZone.run(() => {
